91b0f2979db9b070f8f5d08165b2aa97749fa7f2
Justin Gebert <justin.gebert.berlin@gmail.com>
2022-09-07 16:19:52 +0200

    start timer functions

--- zeiterfassung/index.ts
+++ zeiterfassung/index.ts
@@ -1,23 +1,92 @@
-let start = Date.now();
+import * as XLSX from 'xlsx';
 
-let maschine;
-let arbeitkraft;
-let arbeitschritt;
+import * as fs from 'fs';
+XLSX.set_fs(fs);
 
+import { Readable } from 'stream';
+XLSX.stream.set_readable(Readable);
 interface Zeit{
     
 }
 
 class Zeit{
 
-    maschine: number;
+    arbeitsplatz: number = document.getElementById();
     arbeitkraft: string;
-    arbeitschritt: string;
+    arbeitschritt: string;  
+    sollmenge: number;
+    istmenge: number;
 
-    constructor(){
 
+
+    zeit: number;
+    startzeit: number;
+    startzeitpause: number;
+    pausenzeit : number;
+    running = false;
+
+    constructor(arbeitsplatz:number, arbeitkraft: string, 
+                arbeitschritt: string,sollmenge: number){
+                    this.arbeitsplatz = arbeitsplatz;
+                    this.arbeitkraft = arbeitkraft;
+                    this.arbeitschritt = arbeitschritt;
+                    this.sollmenge = sollmenge;
+
+    }
+
+    startTimer(): void{
+        if(this.running === true){
+            console.log("timer already running");
+        }else{
+            let start = Date.now();
+            this.startzeit = start;
+            this.running = true;
+            console.log("timer started")
+        }
+        
+    }
+
+    stopTimer(): void{
+        if(this.running === false){
+            console.log("no timer running");
+        }else{
+            let zeit = Date.now() - this.startzeit - this.pausenzeit;
+            this.zeit = zeit;
+            console.log("timer stopped")
+        }
+        
+    }
+
+    pauseTimer(): void{
+        if(this.running === false){
+            console.log("timer not running")
+        }else{
+            let pausestart = Date.now();
+            this.startzeitpause = pausestart;
+            console.log("timer paused")
+        }
+        
+    }
+
+    resumeTimer(): void{
+        if(this.running === true){
+            console.log("timer running")
+        }else{
+            let pausenzeit = Date.now() - this.startzeitpause;
+        console.log("timer resumed")
+        }
+    }
+
+    getTime(): number{
+        return this.zeit
     }
 }
 
-console.log("test");
+function initTimer(){
+    let timer01 = new Zeit();
+    
+}
+
 console.log(start);
+
+document.getElementById("startbutton").addEventListener("click",initTimer);
