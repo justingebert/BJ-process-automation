cf0219916b3d0fec69eaa9aed08c0f96965440e3
JustinGebertDesk <justin.gebert.berlin@gmail.com>
2022-11-15 15:20:33 +0100

    docntinue learning documentation / fix bugs

--- zeiterfassung/data.ts
+++ zeiterfassung/data.ts
@@ -6,3 +6,10 @@ const Arbeitskraefte = new Map([
     ['name03', 'xx03'],
     ['name04', 'xx04'],
 ]);
+
+
+//Arbeitsplatz : 2 stellig yahl e.g. 05
+//Arbeitskraft: 2stellige zahl e.g. 44
+//Auftragsnummer: zwei Buchstaben-7 zahlen e.g. AA-4653446
+//ModellNummer: zahl 5 stellig -> vornullen
+//Arbeitsschritt: 3
