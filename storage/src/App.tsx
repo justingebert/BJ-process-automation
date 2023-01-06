5364b786ad4ecee5adcf7e18eab6e8f8de797f6e
Justin <justin.gebert@student.htw-berlin.de>
2023-01-06 12:16:52 +0100

    first react compoenent

--- storage/src/App.tsx
+++ storage/src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Knr from './Knr';
 
 function App() {
   return (
-    
+    <>
+      <h1>Bagjack</h1>
+      <Knr />
+    </>
   );
 }
 
---
+++ storage/src/Knr.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { text } from 'stream/consumers';
+
+export default function Knr() {
+  return (
+    <>
+      <label>Kistennummer</label>
+      <input type="text" placeholder="Kistennummer"/>
+    </>
+  );
+}
